725aa63bc574d75cc311cbc10fa8d19be693df6b
jeffreygreen <mrjeffreygreen@gmail.com>
2022-05-11 16:47:28 -0500

    Brief styling to about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,3 +1,4 @@
+import "../components/styles/About.css"
 import { useState, useEffect } from "react";
 
 function About(props) {
@@ -22,7 +23,7 @@ function About(props) {
     <div>
       <h2>{about.name}</h2>
       <h3>{about.email}</h3>
-      <p>{about.bio}</p>
+      <p className="bio">{about.bio}</p>
     </div>
   );
 
