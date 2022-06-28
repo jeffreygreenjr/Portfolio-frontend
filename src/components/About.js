05eef4fce69a81a48608db72e3ab16f094cb2b40
jeffreygreen <mrjeffreygreen@gmail.com>
2022-06-27 23:14:39 -0500

    Updated about page

--- src/components/About.js
+++ src/components/About.js
@@ -11,17 +11,18 @@ function About() {
             Full-Stack Engineer with a passion for building and maintaining applications with design and user experience at the forefront.
         </div>
         <div className="SkillsSection">
-            <h3 className="SkillsHeading">LANGUAGES & FRAMEWORKS</h3>
+            <h3 className="SkillsHeading">TECHNOLOGIES</h3>
             <div className="SkillsLogoContainer">
                 <img className="SkillsLogo" src="https://i.imgur.com/VXfU8X6.png" alt="html"/>
                 <img className="SkillsLogo" src="https://i.imgur.com/0S92Td7.png" alt="css"/>
                 <img className="SkillsLogo" src="https://i.imgur.com/v7wGFls.png" alt="js"/>
                 <img className="SkillsLogo" src="https://i.imgur.com/eKTRzmh.png" alt="bootstrap"/>
-                <img className="SkillsLogo" src="https://i.imgur.com/qnnLds9.png" alt="vscode"/>
                 <img className="SkillsLogo" src="https://i.imgur.com/t7tD2GQ.png" alt="python"/>
                 <img className="SkillsLogo" src="https://i.imgur.com/1uKFDkI.png" alt="psql"/>
+                <img className="SkillsLogo" src="https://user-images.githubusercontent.com/102068506/175063942-f99be1be-a8b8-4509-bd19-faa13bfebc5e.png" alt="mongodb"/>
                 <img className="SkillsLogo" src="https://i.imgur.com/9hbw0pb.png" alt="node"/>
                 <img className="SkillsLogo" src="https://i.imgur.com/HJQwz8j.png" alt="react"/>
+                <img className="SkillsLogo" src="https://user-images.githubusercontent.com/102068506/175064231-85d30200-9ea2-4fae-85f1-c10082b66bb7.png" alt="graphql"/>
             </div>
         </div>
         <div className="AboutMisc">
